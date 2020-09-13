3e94ec9c82a9a45dcb7e652152ad3cbf7548aca3
kaustubh sadekar <42736936+kaustubh-sadekar@users.noreply.github.com>
2020-09-14 02:08:28 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,20 +5,25 @@ import os
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'kdlutils/README.rst'), "r") as f:
-    long_description = f.read()
+description = 'This library has several helper functions for deep learning
+
+try:
+    with open(path.join(this_directory, 'kdlutils/README.rst'), "r") as f:
+        long_description = f.read()
+except:
+    long_description = description
 
 setup(
   name = 'kdlutils',         # How you named your package folder
   packages = ['kdlutils'],   # Chose the same as "name"
-  version = '1.4.8',      # Start with a small number and increase it with every change you make
+  version = '1.4.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'This library has several helper functions for deep learning',
+  description = description,
   long_description=long_description,
   long_description_content_type="text/x-rst",
   author = 'kaustubh sadekar',                   # Type in your name
   url = 'https://github.com/kaustubh-sadekar/githubActions',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kaustubh-sadekar/githubActions/archive/v_1.4.8.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kaustubh-sadekar/githubActions/archive/v_1.4.9.tar.gz',    # I explain this later on
   keywords = ['Deep Learning', 'Helper functions'],   # Keywords that define your package best
   package_data={'':['README.rst']},
   classifiers=[
