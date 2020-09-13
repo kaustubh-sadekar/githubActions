c60593bdcddd768c0af396771178f4edcb445da6
kaustubh sadekar <42736936+kaustubh-sadekar@users.noreply.github.com>
2020-09-14 01:17:00 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from os import path
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), "r") as f:
+#with open(path.join(this_directory, 'README.rst'), "r") as f:
+os.system("ls")
+with open('README.rst', "r") as f:
     long_description = f.read()
 
 setup(
   name = 'kdlutils',         # How you named your package folder
   packages = ['kdlutils'],   # Chose the same as "name"
-  version = '1.4.3',      # Start with a small number and increase it with every change you make
+  version = '1.4.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library has several helper functions for deep learning',
   long_description=long_description,
   long_description_content_type="text/x-rst",
   author = 'kaustubh sadekar',                   # Type in your name
   url = 'https://github.com/kaustubh-sadekar/githubActions',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kaustubh-sadekar/githubActions/archive/v_1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kaustubh-sadekar/githubActions/archive/v_1.4.4.tar.gz',    # I explain this later on
   keywords = ['Deep Learning', 'Helper functions'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
